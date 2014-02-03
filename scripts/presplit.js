589888015ef833e9c7ccf452a3f7e2619d6dddcc
Asya Kamsky <asya@10gen.com>
2014-02-03 12:41:35 -0800

    Add handling strings and objectIds

--- scripts/presplit.js
+++ scripts/presplit.js
@@ -1,3 +1,6 @@
+var debug=true;
+var minchar=32;
+var maxchar=122;
 /* converts v of type tp to number 
 hexString = yourNumber.toString(16);
 yourNumber = parseInt(hexString, 16);
@@ -5,6 +8,7 @@ yourNumber = parseInt(hexString, 16);
 
 convertTo = function( v, tp) {
     var i=0;
+    if (debug) print("*** tp in convertTo " + tp);
     switch(tp) {
        case "numberI": 
              return v.valueOf();
@@ -16,10 +20,13 @@ convertTo = function( v, tp) {
              return v;
              break;
        case "string": 
-             i = parseInt(v);
+             fi=v.charCodeAt(0);
+             se=v.charCodeAt(1);
+             i = fi*10000+se;
+             if (debug) print("Convert " + v + " to " + i + " from " + fi + " " + se);
              break;
        case "ObjectId": 
-             i = parseInt(v.valueOf().slice(0,8), 16);;
+             i = parseInt(v.valueOf().slice(0,8), 16);
              break;
        case "Date": 
              i = parseInt(v.getTime())/1000;
@@ -33,64 +40,87 @@ convertBack = function( i, tp) {
     var v=null;
     switch(tp) {
        case "numberI": 
-             return v;
+             return NumberInt(i);
+             break;
        case "numberL": 
+             return NumberLong(i);
+             break;
        case "number": 
-             return v;
+             return i;
              break;
        case "string": 
-             i = parseInt(v);
+             fi = Math.round(i/10000)
+             se = i-Math.round(fi*10000);
+             if (debug) print("*** se is " + se);
+             if (se>maxchar) se = maxchar;
+             if (se<minchar) se = minchar;
+             v = String.fromCharCode(fi, se);
+             if (debug) print("Convert " + v + " from " + i + " " + fi);
              break;
        case "ObjectId": 
-             i = parseInt(v.valueOf().slice(0,8), 16);;
+             v = i.toString(16) + "0000000000000000";
              break;
        case "Date": 
-             i = parseInt(v.getTime())/1000;
+             v = new Date(v*1000);
              break;
        default: print("Type " + tp + "not supported yet, supported types are number, string, ObjectId, Date and Bin");
     } 
-    return i;
+    return v;
 }
 
-figureOutType = function( v ) { /*
-      if (typeof(v) != "object") return(typeof(v));
-      if (v isinstanceof ObjectId)  return("ObjectId");
-      if (v isinstanceof Date)  return("Date");  */
+figureOutType = function( v ) {
+      var x = typeof(v);
+      if (debug) print("** in figureOutType " + v + " " + x);
+      if (x != "object") return(x);
+      if (v instanceof ObjectId)  return("ObjectId");
+      if (v instanceof Date)  return("Date");
+      if (v instanceof NumberInt) return("numberI");
+      if (v instanceof NumberLong) return("numberL");
 }
 
 /* minv and maxv must be the same type and that type defines what shard keys will be */
 /* if you get it wrong you are screwed */
 presplit = function ( ns, minvalue, maxvalue) {
-    var debug=true;
     if( typeof(minvalue) != typeof(maxvalue) ) { 
        print("Whoa, the types of min and max must be the same and they are " + typeof(minvalue) + " and " + typeof(maxvalue) + "!");
        return;
     }
+    if (debug) print("Before", minvalue, typeof(minvalue),typeof(maxvalue));
     tp = figureOutType(minvalue);
+    if (debug) print("**** tp is " + tp);
     minv = convertTo(minvalue, tp);
     maxv = convertTo(maxvalue, tp);
+    if (tp=="string") {
+       minchar = minvalue.charCodeAt(1);
+       maxchar = maxvalue.charCodeAt(1);
+    }
+    if (debug) print("After", minvalue, typeof(minvalue),typeof(maxvalue), "type saved is ", tp);
     cfg = db.getSiblingDB("config");
     if (cfg.chunks.count({ns:ns})>1) {
         print("You already have more than one chunk!");
-        return;
+        if (!debug) return;
     }
-    if (db.getSiblingDB(ns.split(".")[0]).getCollection(ns.split(".")[1]).count()>0) {
+    var dbname = ns.split(".")[0];
+    var collname = ns.split(".")[1];
+    if (db.getSiblingDB(dbname).getCollection(collname).count()>0) {
         print("This collection is not empty!");
-        return;
-    }
-    if (sh.getBalancerState()) {
-        print("Balancer is running, please turn it off and try again!");
-        return;
+        if (!debug) return;
     }
     numShards = cfg.shards.count();
     shards = [];
     cfg.shards.find({},{_id:1}).toArray().forEach(function(s) { shards.push(s._id); });
     if (cfg.collections.count({_id:ns, dropped:false}) == 0) { 
         print("No sharded collection " + ns + " found!");
-        return;
+        if (!debug) return;
+    }
+    if (sh.getBalancerState()) {
+        print("Balancer is running, please turn it off and try again!");
+        if (!debug) return;
     }
     if (debug) print("Got past all the checks");
-    var key = cfg.collections.findOne({_id:ns, dropped:false}).key;
+    var ckey = cfg.collections.findOne({_id:ns, dropped:false});
+    if (debug && ckey==null) key={"_id":1};
+    else key = ckey.key;
     var first = 1;
     for (var i in key) { 
         if (first == 1) { var P=i; first=false; continue; }
@@ -101,10 +131,10 @@ presplit = function ( ns, minvalue, maxvalue) {
     var step = Math.round((maxv-minv)/(nchunks));
     var start = minv+step;
     var stop = maxv;
-    if (debug) print(nchunks, step, start, stop);
+    if (debug) print("Chunk, step, start, stop", nchunks, step, start, stop);
     for (i=start,s=0; i<stop; i+=step, s++) {
-        /* key[P] = convertBack(i, tp); or i of appropriate type */
-        key[P] = i;
+        key[P] = convertBack(i, tp);
+        /* key[P] = i; */
         toShard = shards[s%numShards];
         print("will be spliting at " + tojson(key) + " and moving to " + toShard);
         var res = sh.splitAt( ns, key );
