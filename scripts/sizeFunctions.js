c085106ce6e9b1c2beaf3e04ebc8bbab527c8bb4
Asya Kamsky <asya@10gen.com>
2014-03-31 12:45:27 -0700

    some enhancements for display and options

--- scripts/sizeFunctions.js
+++ scripts/sizeFunctions.js
@@ -1,3 +1,4 @@
+load(d+"showIndexSizes.js");
 
 show_extent_space = function (obj) {
       var extA=obj.extents;
@@ -16,13 +17,13 @@ show_extent_space = function (obj) {
           totalSize+=esize;
           res.extents.push({ "file":efile, "size":esize });
       }
-      /* print(obj.ns + ": " + obj.extentCount + " extents");
-      print(totalSize, ""+ Math.round(totalSize/(1024*1024)*1000)/1000 +"GBs");
+      print(obj.ns + ": " + obj.extentCount + " extents");
+      print("\tSize: " + totalSize, " ("+ Math.round(totalSize/(1024*1024)*1000)/1000 +"GBs)");
       if (obj.extentCount > 1) {
          if (totalFiles.length > 1) {
-              print("Extents stored across " + totalFiles.length + " files.");
+              print("\t\tExtents stored across " + totalFiles.length + " files.");
          }
-      } */
+      }
       res.ns=obj.ns;
       res.numExtents=obj.extentCount;
       res.numFiles=totalFiles.length;
@@ -30,13 +31,13 @@ show_extent_space = function (obj) {
       return res;
 }
 
-show_db_sizes = function (dbname) {
+show_db_sizes = function (dbname, verbose) {
         var collectionList=db.getSiblingDB(dbname).getCollectionNames();
         var stats=db.getSiblingDB(dbname).stats();
         var statsGB=db.getSiblingDB(dbname).stats(1024*1024*1024);
         files={};
         collectionList.forEach(function(c) {
-             var v=db.getCollection(c).validate(true);
+             var v=db.getSiblingDB(dbname).getCollection(c).validate(true);
              var res=show_extent_space(v);
              res.extents.forEach(function(e) {
                 if ( ! files.hasOwnProperty(e.file) ) {
@@ -52,13 +53,22 @@ show_db_sizes = function (dbname) {
         for (i in files) {
             numExt=files[i].collections.length;
             colls = [];
+            sizes = [];
             files[i].collections.forEach(function(e) {
-               if (colls.indexOf(e.ns) < 0) colls.push(e.ns);
+               if (colls.indexOf(e.ns) < 0) { 
+                  colls.push(e.ns);
+                  sizes.push(e.size);
+               }
             });
             print("File " + i + " size:  \t" + files[i]["size"] + "  \t " + Math.round(files[i]["size"]/(1024*1024*1024)*1000)/1000 +"GBs.  \t This represents " + numExt + " extents in " + colls.length + " different collections");
+            if (verbose) {
+                for (i=0; i<colls.length;i++) {
+                   print("\t"+colls[i]+" ( " + sizes[i] + " )");
+                }
+            }
         }
-        printjson(stats);
-        printjson(statsGB);
+        // printjson(stats);
+        // printjson(statsGB);
 }
 
 printCollStats = function (scale) {
