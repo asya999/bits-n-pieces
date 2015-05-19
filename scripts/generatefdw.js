301e27fc1633f69148801128a78a38c0fb1126d6
Asya Kamsky <asya@mongodb.com>
2015-05-19 17:24:40 -0400

    use full name for subtable fields
    fixes related to using full names for subtable fields and minimizing
    projections.
    always add $ifNull to the field types that need it.
    add check for ”” for numeric types, replace with 0 (maybe should be
    null?)

--- scripts/generatefdw.js
+++ scripts/generatefdw.js
@@ -12,7 +12,7 @@ var allTypes = {
     "numeric" : 1,
     "boolean" : 2,
     "date"    : 3,
-    "timestampgz": 4,
+    "timestamp": 4,
     "varchar" : 5,
     "array"   : 6,
 };
@@ -31,7 +31,7 @@ schema_to_view=function(sch) {
             sv = sv + " " + s[f]["#pgname"] + " AS " + fm[s[f]["#pgname"]] + ",";
          } else if ( f.startsWith("Zip") || f.startsWith("zip") ) {
              sv = sv + ' "' + f + '", ' + ' substr("' + f + '", 1, 5) ' + ' AS "' + f + '_trim5",';
-         } else if (s[f]=="timestampgz") {
+         } else if (s[f]=="timestamp") {
              sv = sv + " " + f + ", " + f + "::date AS " + f + "_as_date,";
          } else {
              sv = sv + " " + JSON.stringify(f) + ",";
@@ -45,7 +45,6 @@ schema_stringify=function(s) {
       for (f in s) {
          if (!s[f].hasOwnProperty("#pgtype")) continue;
          ss = ss + JSON.stringify(f) + " " + s[f]["#pgtype"] + ",";
-         // ss = ss + "\"" + f + "\" " + s[f] + ",";
       }
       return ss.slice(0,-1);
 }
@@ -55,14 +54,13 @@ mapType = function ( t ) {
     debug("mapType: in type " + t);
     if (typeof(t) == "object" && t.hasOwnProperty("#type")) {
       ttype = firstKeyName(t);
-      //t = t["#type"][ttype];
       t=ttype;
     }
     debug("mapType: really type " + t);
     if ( [ "objectid", "text", "string", "category", "null" ].indexOf(t) >= 0 ) return "varchar";
     if ( [ "2d", "2dsphere" ].indexOf(t) >= 0 ) return "numeric[]";
     if ( [ "number" ].indexOf(t) >= 0 ) return "numeric";
-    if ( [ "date" ].indexOf(t) >= 0 ) return "timestamptz";
+    if ( [ "date" ].indexOf(t) >= 0 ) return "timestamp";
     if ( [ "boolean" ].indexOf(t) >= 0 ) return "boolean";
 }
 
@@ -102,10 +100,23 @@ ifnull = function(f, n) {
     if (n==undefined) n=null;
     ifn={};
     ifn["$ifNull"]=[];
-    (ifn["$ifNull"]).push("$"+f);
+    (ifn["$ifNull"]).push(f);
     (ifn["$ifNull"]).push(n);
     return ifn;
-    
+}
+
+ifempty = function(f, n) {
+    if (n==undefined) n=0;
+    ifc={};
+    ifc["$cond"]=[];
+    ifo={};
+    ifo["$eq"]=[];
+    ifo["$eq"].push(f);
+    ifo["$eq"].push("");
+    ifc["$cond"].push(ifo);
+    ifc["$cond"].push(n);  /* maybe should be {"$literal":n} */
+    ifc["$cond"].push(f);
+    return ifc;
 }
 
 geo2d=[];
@@ -118,7 +129,9 @@ prepSchema = function(mschema, dbname, coll, tablename, result) {
     depth++;
     debug(tojson(mschema));
 
+    if (tablename==coll) throw "cannot be passed in tablename same as collection name";
     if (tablename==undefined) tablename=coll;
+
     if (result==undefined) result={};
     if (!result.hasOwnProperty(tablename)) result[tablename]={};
     if (!result[tablename].hasOwnProperty("dbname")) result[tablename]["dbname"]=dbname;
@@ -131,8 +144,8 @@ prepSchema = function(mschema, dbname, coll, tablename, result) {
 
     numRecords=mschema["#count"];
 
-
     for (field in mschema) {
+        if ( ! mschema.hasOwnProperty(field) ) continue;
         if ( '__schema' == field ) continue;  /* skip metadata */
         if ( field.startsWith("#") ) continue;  /* skip metadata */
         debug("doing field ********************** " + field);
@@ -142,7 +155,7 @@ prepSchema = function(mschema, dbname, coll, tablename, result) {
         if ( geo2d.indexOf(field) >= 0 ) {
            debug(field + " is a 2d index geo field!!!");
            currentfield["#type"]="2d";
-           currentfield["#proj"]=ifnull(field, [null,null]);
+           currentfield["#proj"]=ifnull("$"+field, [null,null]);
            delete(currentfield["#array"]);
            result[tablename]["schema"][field]=currentfield;
            result[tablename]["fieldmap"][field+"[1]"]=field+".lon";
@@ -153,8 +166,7 @@ prepSchema = function(mschema, dbname, coll, tablename, result) {
         if ( geo2dsphere.indexOf(field) >= 0 ) {
            debug(field + " is a 2dsphere index geo field!!!");
            currentfield["#type"]="2dsphere";
-           currentfield["#pgname"]=field+".coordinates";
-           currentfield["#proj"]=ifnull(field+".coordinates", [null,null]);
+           currentfield["#proj"]=ifnull("$"+field+".coordinates", [null,null]);
            debug("Going to delete " + field+".type and " + field+".coordinates fields");
            delete(mschema[field+".type"]);
            delete(mschema[field+".coordinates"]);
@@ -170,96 +182,79 @@ prepSchema = function(mschema, dbname, coll, tablename, result) {
            subtable=tablename+"__"+field;
            result[subtable]={};
            result[subtable]["pipe"]=[];
-           result[subtable]["fieldmap"]=[];
            result[subtable]["pipe"].push({"$unwind":'$'+field});
+           result[subtable]["fieldmap"]={};
            fschema={}
-           fschema[coll+"_id"]=mschema["_id"];
-           fschema[coll+"_id"]["#proj"]="$_id";
-           fschema[field]={}
-           fschema[field][coll+"_id"]=mschema["_id"];
-           fschema[field][coll+"_id"]["$proj"]="$_id";
-           for (g in currentfield) if (currentfield.hasOwnProperty(g)) fschema[field][g]=currentfield[g];
-           debug("fschema is " + tojsononeline(fschema));
            if (currentfield["#type"]=="object" || currentfield["#type"].hasOwnProperty("object") ) {
-              result[subtable]["fieldmap"]
+              fschema[field]={}
+              fschema[field]["_id"]=mschema["_id"];
+              fschema[field]["_id"]["#viewname"]=coll+"._id";
+              for (g in currentfield) 
+                 if (currentfield.hasOwnProperty(g) && !g.startsWith("#")) 
+                     fschema[field][field+"."+g]=currentfield[g]; 
               result=prepSchema(fschema[field], dbname, coll, subtable, result);
            } else {
+              fschema["_id"]=mschema["_id"];
+              fschema[field]=currentfield; 
               result=prepSchema(fschema, dbname, coll, subtable, result);
            }
-           delete(currentfield["#type"]);
+           currentfield["#type"]="subtable";
            delete(currentfield);
            continue;
         }
-        if ((currentfield["#type"]=="object" || currentfield["#type"].hasOwnProperty("object"))  && mschema["__schema"]["options"]["flat"]) {  
-           continue;
-        }
-        if (currentfield["#type"]=="object" && !mschema["__schema"]["options"]["flat"]) {  
-           /* current field is an object and flat wasn't true - flatten it ) */
-           function recurse(obj) { 
-              var res={};
-              for (var o in obj) {
-                 if (!obj.hasOwnProperty(o)) continue;
-                 if (((typeof obj[o]) === 'object') && !obj["#array"] ) {
-                    var flatObject = recurse(obj[o]);
-                    for (var x in flatObject) {
-                       if (!flatObject.hasOwnProperty(x)) continue;
-
-                       res[o + '.' + x] = flatObject[x];
-                   }
-                 } else {
-                    res[o] = obj[o];
-                 }
-              }
-              return res;
-           }
-           var flatobj = recurse(currentfield);
-           for (var f in flatobj) {
-              // only own properties
-              if (!flatobj.hasOwnProperty(f)) continue;
-                  result[tablename]["schema"][f]=flatobj[f];
-           }
+        if (! currentfield.hasOwnProperty("#type")) throw "Field " + field + " does not have #type! just " + tojsononeline(currentfield);
+        if (currentfield["#type"]=="object" || currentfield["#type"].hasOwnProperty("object") ) {  
            delete(currentfield);
            continue;
-        } else {
-           debug("left with field " + field + " currentfield is " + tojsononeline(currentfield));
-           result[tablename]["schema"][field]={};
-           for (g in currentfield) {
-              if (currentfield.hasOwnProperty(g)) result[tablename]["schema"][field][g]=currentfield[g];
-           }
+        }
+        debug("left with field " + field + " currentfield is " + tojsononeline(currentfield));
+        result[tablename]["schema"][field]={};
+        for (g in currentfield) {
+           if (currentfield.hasOwnProperty(g)) result[tablename]["schema"][field][g]=currentfield[g];
         }
     }
     proj = {};
     proj["$project"] = {};
     pr = proj["$project"];
     needProj = false;
-    prefix="";
-    if (!topLevel) {
-       needProj=true;
-       prefix=tablename.slice(tablename.indexOf("__")+2);
-    }
+
     sch = result[tablename]["schema"];
+    debug("Table is " + tablename);
+    debug(sch);
     for (f in sch) {
         if (f.startsWith("#") || f.startsWith("__")) continue;
-        if (f.length>62) {
+        /*  unfinished - if the field name isn't legal postgres column then we need to do some hoop jumping */
+        if (f.length>62) {  /* should check if it already has #pgname? */
            newf=f.replace(/ /g,'').slice(0,62);
            sch[f]["#pgname"]=newf;
            if (!needProj) needProj=true;
-           sch[f]["#proj"]={};
-           sch[f]["#proj"][newf]=ifnull(f);
+           if ( !sch[f].hasOwnProperty("#proj") ) { 
+              sch[f]["#proj"]={};
+              sch[f]["#proj"][newf]=ifnull("$"+f);
+           } else {
+              sch[f]["#proj"][newf]=f; // TEMPORARY
+           }
            result[tablename]["fieldmap"][newf]=f;
         }
         sch[f]["#pgtype"]=mapType(reduceType(sch[f]["#type"]));
         if (f.startsWith("Zip") || f.startsWith("zip")) sch[f]["#pgtype"]="varchar";
+
         debug("field " + f + " was " + tojson(sch[f]["#type"]) + " but turned into " + sch[f]["#pgtype"] );
 
+        prf="$"+f;
         if (sch[f].hasOwnProperty("#proj")) {
+           prf=sch[f]["#proj"];
+        } 
+        if (sch[f]["#pgtype"]!="varchar") {
+           prf=ifnull(prf);
+        } 
+        if (sch[f]["#pgtype"]=="numeric") {
+           prf=ifempty(prf);
+        } 
+
+        if (prf!="$"+f) {
            if (!needProj) needProj=true;
-           pr[f]=sch[f]["#proj"];
-        } else if (sch[f]["#pgtype"]!="varchar") {
-           if (!needProj) needProj=true;
-           pr[f]=ifnull(f);
-        } else if ( !topLevel && f!=prefix ) {
-           pr[f]="$"+prefix+"."+f;
+           pr[f]=prf;
         } else {
            pr[f]=1;
         }
