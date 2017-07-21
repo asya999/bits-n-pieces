37ac67db75a38c1c0b37f2b5b36580df12b7e259
Asya Kamsky <asya999@gmail.com>
2017-07-21 16:52:23 -0400

    concat Arrays helper function

--- scripts/concatArrays.js
+++ scripts/concatArrays.js
@@ -1,3 +1,9 @@
+/*
+ * helper function to concat elements of an array
+ * into a single array - elements may be themselves
+ * arrays in which case they will be "recursively"
+ * concat'ed to i level
+ */
 concatArrays = function (inputArray, i=0) { 
     if (i<0) return inputArray;
     i--;
