34f608958e5d393567c9a313891d3a26d27a8fc4
Asya Kamsky <asya999@gmail.com>
2017-07-21 17:22:07 -0400

    readability

--- scripts/outputKeys.js
+++ scripts/outputKeys.js
@@ -2,7 +2,7 @@
  * given an expression representing an array of K/V pairs,
  * return an expression which outputs all of its keys
  * going to `i` levels deep
- * pre tracks the prefix as we "recurse" down
+ * `prefix` tracks the prefix as we "recurse" down
  *
  */
 outputKeys = function(inputArray, i, pre="") {
@@ -15,13 +15,26 @@ outputKeys = function(inputArray, i, pre="") {
     if (pre!="") sep=".";
     return { "$map": { 
         "input": inputArray, 
-        "in": {$cond:[
-            {$eq:["object",{$type:"$$this.v"}]}, 
-            {$let:{
+        "in": {$cond:{
+            if: {$eq:["object",{$type:"$$this.v"}]}, 
+            then: {$let:{
                 vars: vars,
                 in: outputKeys( {$objectToArray:"$$prev"+i+".v"}, i, {$concat:[pre,sep,"$$prev"+i+".k"]} )
             }},
-            {$concat:[pre, sep, "$$this.k"]}
-        ]} 
+            else: {$cond:{
+                if: {$eq:["array", {$type:"$$this.v"}]},
+                then: {$let:{
+                    vars: vars,
+                    in: {$map:{ 
+                        input:{$filter:{
+                            input:"$$prev"+i+".v", 
+                            cond:{$eq:[{$type:"$$this"},"object"]}
+                        }},
+                        in: outputKeys({$objectToArray:"$$this"}, i, {$concat:[pre,sep,"$$prev"+i+".k"]} )
+                    }}
+                }},
+                else: {$concat:[pre, sep, "$$this.k"]}
+            }}
+        }} 
     }};
 }
