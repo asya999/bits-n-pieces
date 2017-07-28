ece218caeac1f35db85d423dbe6237b6d0e72a3d
Asya Kamsky <asya999@gmail.com>
2017-07-28 17:31:43 -0400

    fix math and formating

--- scripts/printChunkDetails.js
+++ scripts/printChunkDetails.js
@@ -4,7 +4,7 @@ printChunkDetails = function (d) {
    var dbhashTime = db.getSiblingDB(d).runCommand({dbhash:1, collections: ["collections", "chunks"]}).timeMillis; 
    var stats = db.getSiblingDB(d).getCollection('chunks').stats(1024*1024); 
    var indexSize = stats.totalIndexSize;
-   var collSize = Math.round(numchunks*chunksize/1024);
+   var collSize = Math.round(numchunks*chunksize/2/1024);
 
-   print ("chunksize: " + chunksize + "\tNumber of chunks: " + numchunks + "   \tTime to dbhash in millis: " + dbhashTime + "\tSize stats(MBs): " + stats.size + "\tindexes: " + indexSize + "\tAppox coll size(Gbs): " + collSize );
+   print ("chunksize: " + chunksize + "\tNum chunks: " + numchunks + "   \tdbhash Time(millis): " + dbhashTime + "\t Size stats(MBs) data: " + stats.size + " \tindexes: " + indexSize + " \tAppox coll size(Gbs): " + collSize );
 }
