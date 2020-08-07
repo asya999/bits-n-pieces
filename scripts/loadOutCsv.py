6e0fa7acf4577035bfe7d662c80c804793641685
Asya Kamsky <asya999@gmail.com>
2020-08-07 11:12:55 -0400

    updates

--- scripts/loadOutCsv.py
+++ scripts/loadOutCsv.py
@@ -8,8 +8,10 @@ import datetime
 insertNullFields=False 
 
 conn = pymongo.MongoClient(port=27017)
+# db = conn.micron  # dbname
 db = conn.output  # dbname
-docs = db.newout  # collection name
+# docs = db.newout  # collection name
+docs = db.ddb  # collection name
 
 datafile = sys.argv[1:][0]
 nReader = csv.DictReader(open(datafile, "r"), delimiter=',')
@@ -18,11 +20,14 @@ counter=0
 prevCount = 0
 for line in nReader:
    line['tRunning']=counter
+   line['version']="4.0.1"
    counter=counter+1
    line['currCount']=int(line['count'])-prevCount
    prevCount=int(line['count'])
    line['op']=datafile.split('/')[-1].split('.')[0]
-   line['run']=datafile.split('/')[0]+"_"+datafile.split('/')[1]
+   line['db']=datafile.split('/')[0].split('_')[-1]
+   line['dir']=datafile.split('/')[0]
+   line['scenario']=datafile.split('/')[0].split('_')[0]
    for k,v in line.items():
       i=v
       if isinstance(v, basestring) and v.find(".")>0:
